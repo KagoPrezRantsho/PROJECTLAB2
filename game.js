6dd79046aa2d8581715ccf743780c62bb61a1fdb
KagoPrezRantsho <kr2019@hw.ac.uk>
2021-11-03 16:33:58 +0000

    init

--- game.js
+++ game.js
@@ -12,8 +12,8 @@ function Bear() {
     };
 
     this.display = function() { 
-        this.htmlElement.style.left = this.x + "px"; t
-        his.htmlElement.style.top = this.y + "px"; 
+        this.htmlElement.style.left = this.x + "px"; 
+        this.htmlElement.style.top = this.y + "px"; 
         this.htmlElement.style.display = "absolute";
     };
 }
@@ -28,7 +28,6 @@ function start() {
 
 // Handle keyboad events // to move the bear 
 function moveBear(e) {
-
 //codes of the four keys
 const KEYUP = 38; 
 const KEYDOWN = 40; 
@@ -59,8 +58,10 @@ this.fitBounds = function() {
     let w = parent.offsetWidth;
     let h = parent.offsetHeight;
     if (this.x < 0) this.x = 0;
-    if (this.x > w - iw) this.x = w - iw; if (this.y < 0) this.y = 0;
-    if (this.y > h - ih) this.y = h - ih; };
+    if (this.x > w - iw) this.x = w - iw; 
+    if (this.y < 0) this.y = 0;
+    if (this.y > h - ih) this.y = h - ih; 
+};
 
 
 this.move = function(xDir, yDir) {
@@ -84,16 +85,21 @@ function start() {
 
 this.fitBounds = function() {
     let parent = this.htmlElement.parentElement;
-    let iw = this.htmlElement.offsetWidth; let ih = this.htmlElement.offsetHeight; let l = parent.offsetLeft;
+    let iw = this.htmlElement.offsetWidth; 
+    let ih = this.htmlElement.offsetHeight; 
+    let l = parent.offsetLeft;
     let t = parent.offsetTop;
     let w = parent.offsetWidth;
     let h = parent.offsetHeight;
     if (this.x < 0) this.x = 0;
-    if (this.x > w - iw) this.x = w - iw; if (this.y < 0) this.y = 0;
-    if (this.y > h - ih) this.y = h - ih; };
+    if (this.x > w - iw) this.x = w - iw; 
+    if (this.y < 0) this.y = 0;
+    if (this.y > h - ih) this.y = h - ih; 
+};
 
 this.move = function(xDir, yDir) {
-    this.fitBounds(); //we add this instruction to keep bear within board this.x += this.dBear * xDir;
+    this.fitBounds(); //we add this instruction to keep bear within board 
+    this.x += this.dBear * xDir;
     this.y += this.dBear * yDir;
     this.display();
 };
